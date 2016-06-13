5be9bdb8ce266173aacfa2daaaaf9c066e88bf27
Igor Pcholkin <igor.pcholkin@acp.aws.accenture.com>
2016-06-13 18:16:47 +0300

    slight fix

--- src/org/random/sqlcheck/SQLParser.scala
+++ src/org/random/sqlcheck/SQLParser.scala
@@ -74,21 +74,6 @@ object SQLParser extends JavaTokenParsers with ParserUtils with SQLParserHelpers
         (otable2, (ers: EResultSet) => joinTables(ers, column2, column1)))
   }
   
-  def joinTables(ers: EResultSet, column1: (Option[String], String), column2: (Option[String], String)) = {
-    val (otable, field) = column1
-    val (otable2, field2) = column2
-    ers.copy(rs = ers.rs.flatMap { trow =>
-      val row = getRow(otable, trow)
-      val rv = rvalue(row, field, ers)
-      val table2 = otable2.get
-      val rs2 = ers.db(table2)
-      rs2.find( row2 => rv == rvalue(row2, field2, ers)) match {
-        case Some(row2) => Seq(trow + (table2 -> row2))
-        case None => NoRow 
-      }
-    })
-  }
-  
   lazy val greater = (column <~ ">") ~ value ^^ {
     case (otable, field) ~ value => (otable, (ers: EResultSet) => filterValues(otable, ers) { row =>
       value < rvalue(row, field, ers) 
--- src/org/random/sqlcheck/SQLParserHelpers.scala
+++ src/org/random/sqlcheck/SQLParserHelpers.scala
@@ -25,5 +25,20 @@ trait SQLParserHelpers {
     })
   }
   
+  def joinTables(ers: EResultSet, column1: (Option[String], String), column2: (Option[String], String)) = {
+    val (otable, field) = column1
+    val (otable2, field2) = column2
+    ers.copy(rs = ers.rs.flatMap { trow =>
+      val row = getRow(otable, trow)
+      val rv = rvalue(row, field, ers)
+      val table2 = otable2.get
+      val rs2 = ers.db(table2)
+      rs2.find( row2 => rv == rvalue(row2, field2, ers)) match {
+        case Some(row2) => Seq(trow + (table2 -> row2))
+        case None => NoRow 
+      }
+    })
+  }
+  
   
 }
